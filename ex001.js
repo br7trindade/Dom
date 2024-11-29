7cafd1a6300f7bcda78b9877662f8847ffaa5441
brant <brantandbrant>
2024-11-28 21:34:19 -0300

    evoluindo

---
+++ ex001.js
@@ -0,0 +1,13 @@
+function brant() 
+
+{
+    let numero = 3
+    prompt("Digite seu palpite?");
+    
+    if (numero == 3) {
+        alert("numero");
+    } else {
+        alert("Errou");
+    }
+}
+
