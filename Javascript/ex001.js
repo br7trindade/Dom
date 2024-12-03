25493d174cb8b5c0b4aac873da9299c22d6fc808
brant <brantandbrant>
2024-12-02 21:50:19 -0300

    deu errado

---
+++ Javascript/ex001.js
@@ -0,0 +1,19 @@
+
+
+
+function brant() 
+
+{
+    
+    let numero = document.querySelector("input").value
+
+    if (numero == 3) {
+        alert("acertou");
+        console.log("acertou");
+    } 
+    else {
+        alert("Errou");
+        console.log("Errou");
+    }
+}
+
---
+++ Javascript/ex002.js
@@ -0,0 +1,18 @@
+
+
+
+function brant1()
+
+{
+    let ano = document.querySelector("input").value
+
+
+
+    if (ano %4 == 0 && ano %100 != 0){
+        alert("O ano é bissexto");
+    }
+        
+    else {
+          alert("O ano é normal");
+    }
+}
