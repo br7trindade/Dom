f83e62e0b5bbbe3231517adfe893a1c315f6ab9c
brant <leandroyuuki@gmail.com>
2024-12-10 19:43:43 -0300

    jogo numero 4

--- Javascript/ex004.js
+++ Javascript/ex004.js
@@ -1,9 +1,17 @@
 
-
+document.getElementById("botafogo").innerText = ""
 
 function brant4()
 
 {
-    
+    let valor = document.querySelector("input").value
+
+    valor = Number(valor)
+
+    let gorjeta = (valor * 0.10) + valor
+
+    //alert("O valor da conta, com o acréscimo da gorjeta é: " + gorjeta)
+
+    document.getElementById("botafogo").innerText = "O valor da conta, com o acréscimo da gorjeta é: " + gorjeta
 
 }
